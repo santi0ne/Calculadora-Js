873ecb20220c441d625a9677a05b23ec7b4daad0
santi0ne <johsrodr@espol.edu.ec>
2023-06-17 13:16:17 -0500

    subida de proyecto

---
+++ public/javascript/calculadora.js
@@ -0,0 +1,24 @@
+
+/* funcion flecha */
+/* objeto document */ 
+let funcionPredeterminada = () => {
+    const formulario = document.getElementById('formulario')
+    /* action -> 'submit' */
+    /* callback -> evento */
+    formulario.addEventListener('submit', (evento) => {
+        evento.preventDefault(); /* prevenir el evento por default */
+
+        /* definir variables y asignarles el valor ingresado en las cajas de texto del form */
+        let valorPorHora = document.getElementById('valorPorHora').value
+        let interfazHoras = document.getElementById('interfazHoras').value
+        let htmlHoras = document.getElementById('desarrolloHtml').value
+        let jsHoras = document.getElementById('logicJavascript').value
+
+        let resultado = (valorPorHora * interfazHoras) + (valorPorHora * htmlHoras) + (valorPorHora * jsHoras)
+        document.getElementById('valorTotal').value = '$'+resultado
+    })
+  };
+
+/* invocar funcion flecha */
+funcionPredeterminada();
+  
